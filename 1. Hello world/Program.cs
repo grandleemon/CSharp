cb200bfd938d323bf5a9d64e6aaebb1c7e76a97c
Max <maxbond241@gmail.com>
2022-12-16 12:15:45 +0200

    created variable that read input from console, then showed it

--- 1. Hello world/Program.cs
+++ 1. Hello world/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Enter your name:");
+var name = Console.ReadLine();
+Console.WriteLine($"Hello, {name}!");
